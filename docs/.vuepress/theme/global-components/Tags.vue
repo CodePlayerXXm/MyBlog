1b2b1a60200ee2c1c2faa50a7b4e24a0ac4adeba
Ethan <constable1989@163.com>
2021-12-07 14:57:19 +0800

    test

--- docs/.vuepress/theme/global-components/Tags.vue
+++ docs/.vuepress/theme/global-components/Tags.vue
@@ -36,6 +36,7 @@ export default {
         color9: "Git",
         color10: "TypeScript",
         color11: "Webpack",
+        color12: "《你不知道的Javascript》笔记",
       },
     };
   },
@@ -111,4 +112,7 @@ export default {
 
   &.color11
     background-color rgba(141,214,249,1)
+
+  &.color12
+    background-color #EDD93B
 </style>
---
+++ docs/articles/youDontKnowJavascript/Scope.md
@@ -0,0 +1,11 @@
+---
+sidebar: auto
+tag:
+  - 技术
+  - 《你不知道的Javascript》笔记
+features:
+  - title: 《你不知道的Javascript》笔记 -- 作用域
+    details: 作用域
+---
+
+# test
